ef5a9ed78f50369d740c6dc1fe564a040ae81404
Bailey <baileypiggott@gmail.com>
2015-11-16 12:44:05 -0500

    make periodogram of data

--- tf_test.R
+++ tf_test.R
@@ -1,4 +1,25 @@
 # test method of creating a transfer function using simulated data
+#load data and libraries------------
+library(tidyr)
+library(magrittr)
+library(dplyr)
+library(multitaper)
 
 data <- read.table("sim_data.txt")
 colnames(data) <- c('H', 'D', 'Z', 'A')
+
+# create periodogram --------------------
+#plot(data$A, type = 'l')
+
+N = 200
+H <- data$H[1:N]
+A <- data$A[1:N]
+
+spec_H <- (1/N)*abs(fft(H))^2 # spectral estimate
+spec_A <- (1/N)*abs(fft(A))^2 # spectral estimate
+
+# make transfer function ---------------------
+
+
+
+
