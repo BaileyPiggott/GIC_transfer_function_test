b3d6c0184c7aa841af48c2ab2a8bb97e7fb30fc8
Bailey <baileypiggott@gmail.com>
2015-11-22 13:54:19 -0500

    function to generate three component transfer function
    getting negative values for one of the components - don't know if this is an issue with the code or with the data

--- tf_test.R
+++ tf_test.R
@@ -10,6 +10,7 @@ library(ggplot2)
 
 source('get_spec.R') #function to generate spectral estimate
 source('get_tf.R') # function to generate transfer function
+source('get_tf_all.R')
 
 sim_data <- as.data.frame(read.table("sim_data.txt"))
 colnames(sim_data) <- c('H', 'D', 'Z', 'A')
@@ -29,7 +30,7 @@ freq <- seq(1,(N/2+1),2) # frequencies at which to calculate the transfer functi
 
 tf <- get_tf_ind(spec_H_block, spec_D_block, spec_Z_block, spec_A_block, freq)
 
-#tf_all <- get_tf_all()
+tf_all <- get_tf_all(spec_H_block, spec_D_block, spec_Z_block, spec_A_block, freq)
 
 #plot transfer functions ----------------------------
 
@@ -42,10 +43,10 @@ ggplot(data = tf, aes(x = freq, y = val)) +
 #facet plot of all three transfer functions
 # assuming A is dependant on all variables
 
-tf_all <- data.frame('freq' = freq, 'H' = M_test[,1], 'D' = M_test[,2], 'Z' = M_test[,3]) %>% 
+tf_three <- data.frame('freq' = freq, 'H' = tf_all[1,], 'D' = tf_all[2,], 'Z' = tf_all[3,]) %>% 
   gather(tf, val, 2:4)
 
-ggplot(data = tf_all, aes(x = freq, y = val)) +
+ggplot(data = tf_three, aes(x = freq, y = val)) +
   facet_grid(tf~.) +
   geom_line() +
-  labs(title = paste0("Transfer Functions\n Block Length = ", N, " samples"), x = "Frequency", y = "")
+  labs(title = paste0("Transfer Functions\n Block Length = ", N, " samples\n", "Sampled at ", length(freq), " Frequencies"), x = "Frequency", y = "")
